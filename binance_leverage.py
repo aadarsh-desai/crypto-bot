73e983c5780be3c71c9ef4c25cd95340d8620314
aadarsh-desai <aadarshdesai455@gmail.com>
2025-03-23 16:43:06 +0530

    Update binance_leverage.py

--- binance_leverage.py
+++ binance_leverage.py
@@ -2,14 +2,16 @@ from binance.client import Client
 from dotenv import load_dotenv
 import os
 
+# Load environment variables
 load_dotenv()
-api_key = os.getenv("6c99a27e5727d38011a37f9ccec4a31a581a9bb7c3c7e8ecf054d7ba1da3abfa")
-api_secret = os.getenv("7c2fde9aa087dc224f6488f31873ff32a67f786ca387bea81e2fc70cbde75f78")
+
+api_key = os.getenv("BINANCE_API_KEY")
+api_secret = os.getenv("BINANCE_API_SECRET")
 
 client = Client(api_key, api_secret)
 
 # Set leverage for futures
-symbol = 'BTCUSDT'
+symbol = 'SOLUSDT'  # or 'SOLUSDT' as per your choice
 leverage = 20
 response = client.futures_change_leverage(symbol=symbol, leverage=leverage)
 print(response)
